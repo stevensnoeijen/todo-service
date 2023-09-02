74dd103afafa978754b75d6fd1e01a5a8d8f87ac
Steven Snoeijen <stevensnoeijen@gmail.com>
2023-09-02 14:35:25 +0200

    feat: add typeorm with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DB_URL'),
+      }),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
